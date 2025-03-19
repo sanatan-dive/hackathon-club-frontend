0dc6772cd42a5b36f4433c5f4ea904ec1a6d161a
sanatan-dive <sanatansharma352@gmail.com>
2025-03-19 14:06:13 +0530

    responsiveness done

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -33,7 +33,7 @@ export default function Carousel() {
       className="relative w-full z-10"
       data-carousel="static"
     >
-      <div className="relative rounded-md h-48 md:h-96 lg:h-[540px] overflow-hidden">
+      <div className="relative rounded-md h-32 sm:h-48 md:h-96 lg:h-[540px] overflow-hidden">
         {carouselItems.map((src, index) => (
           <div
             className={`transition-opacity duration-700 ease-in-out ${
@@ -43,20 +43,22 @@ export default function Carousel() {
           >
             <img
               src={src}
-              className="absolute block w-full h-full object-cover"
+              className="absolute block w-full h-full object-cover object-center"
               alt={`Slide ${index + 1}`}
             />
           </div>
         ))}
       </div>
+
+      {/* Previous Button */}
       <button
         type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-1/2 -translate-y-1/2 start-0 z-30 flex items-center justify-center px-2 sm:px-4 cursor-pointer group focus:outline-none"
         onClick={prevSlide}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-stone-800/30 group-hover:bg-white/50 dark:group-hover:bg-stone-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-stone-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-white/30 dark:bg-stone-800/30 group-hover:bg-white/50 dark:group-hover:bg-stone-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-stone-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-stone-800 rtl:rotate-180"
+            className="w-3 h-3 sm:w-4 sm:h-4 text-white dark:text-stone-800 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -73,14 +75,16 @@ export default function Carousel() {
           <span className="sr-only">Previous</span>
         </span>
       </button>
+
+      {/* Next Button */}
       <button
         type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-1/2 -translate-y-1/2 end-0 z-30 flex items-center justify-center px-2 sm:px-4 cursor-pointer group focus:outline-none"
         onClick={nextSlide}
       >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-stone-800/30 group-hover:bg-white/50 dark:group-hover:bg-stone-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-stone-800/70 group-focus:outline-none">
+        <span className="inline-flex items-center justify-center w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-white/30 dark:bg-stone-800/30 group-hover:bg-white/50 dark:group-hover:bg-stone-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-stone-800/70 group-focus:outline-none">
           <svg
-            className="w-4 h-4 text-white dark:text-stone-800 rtl:rotate-180"
+            className="w-3 h-3 sm:w-4 sm:h-4 text-white dark:text-stone-800 rtl:rotate-180"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
